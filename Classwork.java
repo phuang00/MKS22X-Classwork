0d17df32e9ed6ea44a8b515c4ab3e1b89b026346
phuang00 <phuang00@stuy.edu>
2019-02-04 11:28:43 -0500

    wrote makeAllWords for three letters and tested it in main

--- Classwork.java
+++ Classwork.java
@@ -8,10 +8,19 @@ public class Classwork{
   }
 
   private static void helper(int k, List<String> l, String word){
-
+    if (k == 0) l.add(word);
+    else{
+      for (int i = 0; i < 3; i++){
+        helper(k - 1, l, word + (char)('a' + i));
+      }
+    }
   }
 
   public static void main(String[] args) {
+    System.out.println(makeAllWords(0));
+    System.out.println(makeAllWords(1));
+    System.out.println(makeAllWords(2));
+    System.out.println(makeAllWords(3));
 
   }
 }
