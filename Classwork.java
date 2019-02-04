a3976b86cb093d417a102d62417ac52beb3ac6d1
phuang00 <phuang00@stuy.edu>
2019-02-04 11:26:10 -0500

    wrote skeleton for makeAllWords method and helper

---
+++ Classwork.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Classwork{
+
+  public static List<String> makeAllWords(int k){
+    ArrayList<String> ans = new ArrayList<>();
+    helper(k, ans, "");
+    return ans;
+  }
+
+  private static void helper(int k, List<String> l, String word){
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
